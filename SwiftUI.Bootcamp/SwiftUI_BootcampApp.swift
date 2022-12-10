19f83450f0f90bc9a9801c07c1e667ec6dc4724a
Damian <hello@damianruta.pl>
2022-12-10 21:38:27 +0100

    Initial Commit

---
+++ SwiftUI.Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI.Bootcamp
+//
+//  Created by Damian Ruta on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
